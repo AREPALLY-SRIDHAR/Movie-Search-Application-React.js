609bf3dae5360479e2b9d0a4d27919410f8d83ed
AREPALLY-SRIDHAR <124271276+AREPALLY-SRIDHAR@users.noreply.github.com>
2023-05-05 14:16:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=QPistcpGB8o',
     categoryId: 'ACTION',
     name: 'The Tomorrow war',
+    year: '2021',
   },
   {
     id: '612271842cad3c2dfcb82481',
@@ -18,6 +19,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=k397HRbTtWI&t=1s',
     categoryId: 'COMEDY',
     name: 'The Boss baby',
+    year: '2017',
   },
   {
     id: '6122709941329807a4e17b39',
@@ -25,6 +27,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=eOrNdBpGMv8',
     categoryId: 'ACTION',
     name: 'Avengers',
+    year: '2012',
   },
   {
     id: '61227184c889cff17f05900b',
@@ -32,6 +35,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=ZU3Xban0Y6A',
     categoryId: 'COMEDY',
     name: 'The Intern',
+    year: '2015',
   },
   {
     id: '612271846f711783024f2bfa',
@@ -39,6 +43,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=IUCeN7kelXs',
     categoryId: 'COMEDY',
     name: 'Karwaan',
+    year: '2018',
   },
   {
     id: '612271141bf93653809cdccb',
@@ -46,6 +51,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=tQ0mzXRk-oM',
     categoryId: 'ACTION',
     name: 'War',
+    year: '2019',
   },
   {
     id: '61227184c7ac22c8258938c5',
@@ -53,6 +59,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=dDh1l3qVNoY',
     categoryId: 'COMEDY',
     name: 'Yes Man',
+    year: '2008',
   },
   {
     id: '61227184ae30e00e3ce542c8',
@@ -61,6 +68,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=YVYzxm_RqMg&t=9s',
     categoryId: 'COMEDY',
     name: 'Isnt it romantic',
+    year: '2019',
   },
   {
     id: '6122718418ae122e517c3ada',
@@ -69,6 +77,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=fo9EhcwQXcM',
     categoryId: 'COMEDY',
     name: 'Total Dhamaal',
+    year: '2007',
   },
   {
     id: '61227099ce46ed88e7f00c19',
@@ -77,6 +86,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=odM92ap8_c0',
     categoryId: 'ACTION',
     name: 'Godzilla vs Kong',
+    year: '2021',
   },
   {
     id: '612271848b2be0f2f769d24a',
@@ -85,6 +95,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=lMAj8tyThw0',
     categoryId: 'COMEDY',
     name: 'Welcome to Newyork',
+    year: '2018',
   },
   {
     id: '61227184882da0a972890152',
@@ -92,6 +103,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=3-7jehmURuM',
     categoryId: 'COMEDY',
     name: 'Pagalpanti',
+    year: '2019',
   },
   {
     id: '6122709931ad5e69f5125806',
@@ -99,6 +111,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=P2g94xQmtHw',
     categoryId: 'ACTION',
     name: 'Gamer',
+    year: '2009',
   },
   {
     id: '61227099d3c5b18801b2edb5',
@@ -107,6 +120,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=xaPLUII_M6g',
     categoryId: 'ACTION',
     name: 'Rule of the Law',
+    year: '2012',
   },
   {
     id: '61227114b7b5232771c68883',
@@ -114,6 +128,7 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=IYfdQOGqL1o',
     categoryId: 'ACTION',
     name: 'Priest',
+    year: '2011',
   },
   {
     id: '61227099ea52349bd1e16ee4',
@@ -122,6 +137,168 @@ const moviesList = [
     videoUrl: 'https://www.youtube.com/watch?v=EDGYVFZxsXQ',
     categoryId: 'ACTION',
     name: 'Ghost Protocal',
+    year: '2011',
+  },
+  {
+    id: '11',
+    thumbnailUrl:
+      'https://wallpapers.com/images/high/rrr-movie-trailer-poster-u79atg08s19esbf6.webp',
+    name: 'RRR',
+    year: '2022',
+  },
+  {
+    id: '12',
+    thumbnailUrl:
+      'https://w0.peakpx.com/wallpaper/70/526/HD-wallpaper-rocking-star-yash-kgf-chapter-2.jpg',
+    name: 'KGF2',
+    year: '2022',
+  },
+  {
+    id: '13',
+    thumbnailUrl:
+      'https://images.news18.com/ibnlive/uploads/2021/01/1611818353_pushpa-1.jpg',
+    name: 'Pusha',
+    year: '2022',
+  },
+  {
+    id: '14',
+    thumbnailUrl: 'https://images.indianexpress.com/2018/11/2-0-7595.jpg',
+    name: 'Robo',
+    year: '2010',
+  },
+  {
+    id: '15',
+    thumbnailUrl: 'https://wallpapercave.com/wp/wp4027473.jpg',
+    name: 'Bahubali',
+    year: '2015',
+  },
+  {
+    id: '16',
+    thumbnailUrl:
+      'https://c4.wallpaperflare.com/wallpaper/621/286/348/keanu-reeves-keanu-reeves-parabellum-john-wick-john-wick-hd-wallpaper-preview.jpg',
+    name: 'Jhon Wick',
+    year: '2014',
+  },
+  {
+    id: '17',
+    thumbnailUrl:
+      'https://e1.pxfuel.com/desktop-wallpaper/733/587/desktop-wallpaper-avatar-fantasy-action-adventure-sci-avatar-poster.jpg',
+    name: 'Avatar',
+    year: '2009',
+  },
+  {
+    id: '18',
+    thumbnailUrl:
+      'https://gumlet.assettype.com/dtnext%2F2023-02%2Fa96839c1-f132-4b1e-8b64-6ec699357c42%2Fdtnext_2022_10_5d588f84_54a8_4807_b043_1a2e1f70b39f_123.avif?rect=0%2C0%2C999%2C562&auto=format%2Ccompress&fit=max&format=webp&w=400&dpr=2.6',
+    name: 'PS2',
+    year: '2023',
+  },
+  {
+    id: '19',
+    thumbnailUrl:
+      'https://e.telugurajyam.com/wp-content/uploads/2023/03/nani-dasara-theatrical-trailer-on-march-14th.jpg',
+    name: 'Dasara',
+    year: '2023',
+  },
+  {
+    id: '20',
+    thumbnailUrl:
+      'https://e1.pxfuel.com/desktop-wallpaper/676/496/desktop-wallpaper-vikram-movie-kamal-haasan-vikram-movie.jpg',
+    name: 'Vikram',
+    year: '2022',
+  },
+  {
+    id: '21',
+    thumbnailUrl:
+      'https://w0.peakpx.com/wallpaper/320/432/HD-wallpaper-batman-movie-the-dark-knight-rises.jpg',
+    name: ' Dark Knight Rises',
+    year: '2012',
+  },
+  {
+    id: '22',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/645142.jpg',
+    name: 'Thor',
+    year: '2011',
+  },
+  {
+    id: '23',
+    thumbnailUrl:
+      'https://c4.wallpaperflare.com/wallpaper/128/523/610/movie-train-to-busan-wallpaper-preview.jpg',
+    name: 'Train to busan',
+    year: '2016',
+  },
+  {
+    id: '24',
+    thumbnailUrl: 'https://cdn.wallpapersafari.com/36/30/kg5x4R.jpg',
+    name: 'Everthing everywhere',
+    year: '2022',
+  },
+  {
+    id: '25',
+    thumbnailUrl:
+      'https://images.hdqwalls.com/wallpapers/tenet-movie-4k-7x.jpg',
+    name: 'Tenet',
+    year: '2020',
+  },
+  {
+    id: '26',
+    thumbnailUrl:
+      'https://e1.pxfuel.com/desktop-wallpaper/314/152/desktop-wallpaper-tanhaji-the-unsung-warrior-akshay-kumar-pens-a-heartfelt-note-for-friend-ajay-devgn-as-he-shares-his-fiery-new-poster-tanhaji-movie.jpg',
+    name: 'Tanhaji',
+    year: '2020',
+  },
+  {
+    id: '27',
+    thumbnailUrl: 'https://i.ytimg.com/vi/w8OyVvUnAAg/maxresdefault.jpg',
+    name: 'Sherlock holmes',
+    year: '2009',
+  },
+  {
+    id: '28',
+    thumbnailUrl: 'https://i.ytimg.com/vi/xyjTHc-eWSs/maxresdefault.jpg',
+    name: 'Interstellar',
+    year: '2009',
+  },
+  {
+    id: '29',
+    thumbnailUrl:
+      'https://www.escapistmagazine.com/wp-content/uploads/2020/07/inception1.jpg',
+    name: 'Inception',
+    year: '2010',
+  },
+  {
+    id: '30',
+    thumbnailUrl:
+      'https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/karthikeya-2-et00307869-1667801965.jpg',
+    name: 'Karthikeya 2',
+    year: '2022',
+  },
+  {
+    id: '31',
+    thumbnailUrl:
+      'https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/02/Shutter-Island-Ending-Explained.jpg',
+    name: 'Shutter Island',
+    year: '2010',
+  },
+  {
+    id: '32',
+    thumbnailUrl:
+      'https://m.media-amazon.com/images/S/pv-target-images/33681988eff05c3250cbbfde6543e8f1c88b2fd493e4f16f7d43cd5f9d25d5c3._UR1920,1080_.jpg',
+    name: 'Wolf of wall street',
+    year: '2013',
+  },
+  {
+    id: '33',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/3470347.jpg',
+    name: 'Troy',
+    year: '2004',
+  },
+  {
+    id: '34',
+    thumbnailUrl:
+      'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/93f5ec51361c08f09e139e7272322cd446538c0f22a70b67dfd1f9026af8f9f5._RI_V_TTW_.jpg',
+    name: 'Eega',
+    year: '2012',
   },
 ]
 
